3f805c896d308190b8feb631d829294ef4cf30f2
padeb185 <pierre-andre@hotmail.be>
2025-12-13 17:57:04 +0100

    déclaration classes métiers

--- dBSolutionV3/car_brand/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarBrandConfig(AppConfig):
-    name = 'car_brand'
--- dBSolutionV3/car_specimen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CarSpecimenConfig(AppConfig):
-    name = 'car_specimen'
