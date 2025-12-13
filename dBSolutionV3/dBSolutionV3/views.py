1941f01c0f4aa79e6ca951c8d10744e6c6f0d925
padeb185 <pierre-andre@hotmail.be>
2025-12-13 13:30:47 +0100

    base opérationnelle

--- dBSolutionV3/dBSolutionV3/views.py
+++ dBSolutionV3/dBSolutionV3/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.utils.translation import gettext as _
 
 def home(request):
-    message = _("Welcome to the platform")
+    message = _("Bienvenue sur notre site")
     return render(request, "home.html", {"message": message})
 
 
 def dashboard(request):
-    message = _("Welcome to the dashboard")
+    message = _("Bienvenue sur ton tableau de bord")
     return render(request, 'dashboard.html', {'message': message})
