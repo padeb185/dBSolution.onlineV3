dcee6730f19f6189c2485f874209957ff1f04e77
padeb185 <pierre-andre@hotmail.be>
2025-12-24 14:27:19 +0100

    accueil

--- dBSolutionV3/dBSolutionV3/urls.py
+++ dBSolutionV3/dBSolutionV3/urls.py
@@ -6,22 +6,24 @@ from authentification.views import login_totp, totp_setup
 from dBSolutionV3.views import dashboard, home
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
+    path('i18n/', include('django.conf.urls.i18n')),  # pour changer la langue
 ]
 
 urlpatterns += i18n_patterns(
     # Admin
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
 
-    # Pages de login / TOTP
+    # Page d'accueil
+    path('', home, name='home'),
+
+    # Login standard et TOTP
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('login/totp/', login_totp, name='login_totp'),
     path('login/totp/setup/', totp_setup, name='totp_setup'),
 
-    # Dashboard / home
-    path('', home, name='home'),
+    # Dashboard
     path('dashboard/', dashboard, name='dashboard'),
 
-    # Inclure toutes les autres URLs de l’app authentification
-    path('', include('authentification.urls')),
+    # Autres URLs de l'app authentification
+    path('auth/', include('authentification.urls')),
 )
