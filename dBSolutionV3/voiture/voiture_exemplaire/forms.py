993ada1c49409c5a0d9cb738122e0b6a85312f47
padeb185 <pierre-andre@hotmail.be>
2026-01-17 11:18:24 +0100

    moteur suite

--- dBSolutionV3/voiture/voiture_exemplaire/forms.py
+++ dBSolutionV3/voiture/voiture_exemplaire/forms.py
@@ -4,11 +4,12 @@ from .models import VoitureExemplaire
 class VoitureExemplaireForm(forms.ModelForm):
     class Meta:
         model = VoitureExemplaire
-        fields = '__all__'  # inclut tous les champs
+        fields = '__all__'
 
         widgets = {
             "annee_production": forms.NumberInput(attrs={"min": 1900, "max": 2100, "class": "w-full border border-gray-300 rounded-lg px-4 py-3 text-base"}),
             "mois_production": forms.NumberInput(attrs={"min": 1, "max": 12, "class": "w-full border border-gray-300 rounded-lg px-4 py-3 text-base"}),
             "date_mise_en_circulation": forms.DateInput(attrs={"type": "date", "class": "w-full border border-gray-300 rounded-lg px-4 py-3 text-base"}),
-            # tu peux ajouter des widgets similaires pour d'autres champs si tu veux
+            "date_derniere_intervention": forms.DateInput(attrs={"type": "date"}),
         }
+
