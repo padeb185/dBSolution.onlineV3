8559a710e9eb486922e672fd63c262b31d2702e5
padeb185 <pierre-andre@hotmail.be>
2026-01-29 13:59:42 +0100

    maintenance

--- dBSolutionV3/maintenance/views.py
+++ dBSolutionV3/maintenance/views.py
@@ -5,7 +5,7 @@ from voiture.voiture_exemplaire.models import VoitureExemplaire
 from voiture.voiture_modele.models import VoitureModele
 from maintenance.models import Maintenance
 from maintenance.models import TypeMaintenance
-
+from django.utils.translation import gettext as _
 
 @login_required
 def liste_maintenance_all(request):
@@ -35,7 +35,17 @@ def detail_exemplaire_maintenance(request, exemplaire_id):
         "exemplaire": exemplaire,
     })
 
-
+# Types possibles de maintenance
+TYPES_MAINTENANCE = [
+    {"code": "checkup", "nom": _("Checkup")},
+    {"code": "entretien", "nom": _("Entretien")},
+    {"code": "freins", "nom": _("Freins")},
+    {"code": "pneus", "nom": _("Pneus")},
+    {"code": "nettoyage_ext", "nom": _("Nettoyage extérieur")},
+    {"code": "nettoyage_int", "nom": _("Nettoyage intérieur")},
+    {"code": "niveaux", "nom": _("Niveaux")},
+    {"code": "autres", "nom": _("Autres interventions")},
+]
 
 @login_required
 def choisir_type_maintenance(request, exemplaire_id):
@@ -43,19 +53,22 @@ def choisir_type_maintenance(request, exemplaire_id):
 
     if request.method == "POST":
         type_choisi = request.POST.get("type_maintenance")
+        date_intervention = request.POST.get("date_intervention")
         description = request.POST.get("description", "")
-        if type_choisi:
+
+        if type_choisi and date_intervention:
             Maintenance.objects.create(
                 voiture_exemplaire=exemplaire,
                 type_maintenance=type_choisi,
                 immatriculation=exemplaire.immatriculation,
-                date_intervention=request.POST.get("date_intervention"),
+                date_intervention=date_intervention,
                 description=description
             )
+            # Redirige vers la liste de maintenance du modèle
             return redirect('maintenance:list', modele_id=exemplaire.voiture_modele.id)
 
     context = {
         "exemplaire": exemplaire,
-        "types_maintenance": TypeMaintenance.choices
+        "types_maintenance": TYPES_MAINTENANCE
     }
     return render(request, "maintenance/choisir_type.html", context)
