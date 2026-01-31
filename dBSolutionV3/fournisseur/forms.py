a663111dc2912c618fc03e5e49af300106c3e2e3
padeb185 <pierre-andre@hotmail.be>
2026-01-31 13:19:07 +0100

    fournisseur modifier

--- dBSolutionV3/fournisseur/forms.py
+++ dBSolutionV3/fournisseur/forms.py
@@ -7,6 +7,32 @@ class FournisseurForm(forms.ModelForm):
         model = Fournisseur
         fields = "__all__"
         widgets = {
-            "taux_tva": forms.NumberInput(attrs={"step": "0.01"}),
-            "country_code": forms.TextInput(attrs={"maxlength": 2}),
+            "peppol_id": forms.TextInput(attrs={
+                "placeholder": "0208:BE0123456789"
+            }),
+            "country_code": forms.TextInput(attrs={
+                "placeholder": "BE"
+            }),
+            "email": forms.EmailInput(attrs={
+                "placeholder": "contact@fournisseur.be"
+            }),
+            "telephone_fixe": forms.TextInput(attrs={
+                "placeholder": "+32 2 123 45 67"
+            }),
+            "gsm": forms.TextInput(attrs={
+                "placeholder": "+32 4 123 45 67"
+            }),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # 👉 SI on est en modification
+        if self.instance and self.instance.pk:
+            for field in self.fields.values():
+                # Supprimer les help_text
+                field.help_text = None
+
+                # Supprimer les placeholders
+                if "placeholder" in field.widget.attrs:
+                    field.widget.attrs.pop("placeholder")
