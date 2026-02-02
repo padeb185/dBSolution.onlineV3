4efeef12ef532404ea25dc0aa2393de6496d8e6e
padeb185 <pierre-andre@hotmail.be>
2026-02-02 10:06:09 +0100

    client

--- dBSolutionV3/client/forms.py
+++ dBSolutionV3/client/forms.py
@@ -5,4 +5,18 @@ from .models import Client
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
-        fields = "__all__"
+        fields = [
+            "prenom",
+            "nom",
+            "adresse",
+            "numero_telephone",
+            "numero_permis",
+            "numero_carte_id",
+            "numero_compte",
+            "numero_carte_bancaire",
+            "email",
+            "date_naissance",
+            "niveau",
+            "historique",
+            "location",
+        ]
