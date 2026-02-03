2299cdd4c2ade83bfb51e8a79f33f92308224890
padeb185 <pierre-andre@hotmail.be>
2026-02-03 13:42:38 +0100

    intervention carrosserie

--- dBSolutionV3/carrosserie/urls.py
+++ dBSolutionV3/carrosserie/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import CarrosserieListView, carrosserie_detail, ajouter_carrosserie_all, modifier_carrosserie
+from .views import CarrosserieListView, carrosserie_detail, ajouter_carrosserie_all, modifier_carrosserie, \
+    InterventionListView
 
 app_name = "carrosserie"
 
@@ -14,6 +15,28 @@ urlpatterns = [
         "carrosserie/creer/",
         ajouter_carrosserie_all,
         name="carrosserie_create",
+    ),
+
+path(
+        "<uuid:carrosserie_id>/",
+        carrosserie_detail,
+        name="carrosserie_detail",
+    ),
+
+    path(
+        'carrosserie/<uuid:carrosserie_id>/modifier/',
+        modifier_carrosserie,
+        name='modifier_carrosserie'),
+
+    path(
+        "intervention/",
+        InterventionListView.as_view(),
+        name="carrosserie_list",
+    ),
+    path(
+        "information/creer/",
+        ajouter_intervention_all,
+        name="intervention_create",
     ),
 
 path(
