bf4d62cd8be729ecd2cce0b577c11e43d06e6917
padeb185 <pierre-andre@hotmail.be>
2026-02-12 18:02:54 +0100

    fuel

--- dBSolutionV3/fuel/admin.py
+++ dBSolutionV3/fuel/admin.py
@@ -2,13 +2,16 @@ from django.contrib import admin
 from django.db.models import Sum
 from .models import Fuel
 
-
 @admin.register(Fuel)
 class FuelAdmin(admin.ModelAdmin):
-    list_display = ('voiture_exemplaire', 'date', 'litres', 'total_litres_vehicule')
+    list_display = ('voiture_exemplaire', 'date', 'litres', 'total_litres_vehicule', 'total_prix_all')
 
-    # Méthode pour afficher le total des litres pour ce véhicule
+    # Total des litres pour ce véhicule
     def total_litres_vehicule(self, obj):
         return Fuel.total_litres_mois(obj.voiture_exemplaire)
-
     total_litres_vehicule.short_description = "Total Litres"
+
+    # Total du prix pour ce véhicule
+    def total_prix_all(self, obj):
+        return Fuel.objects.filter(voiture_exemplaire=obj.voiture_exemplaire).aggregate(total=Sum('prix_refuelling'))['total'] or 0
+    total_prix_all.short_description = "Total Prix Litres"
