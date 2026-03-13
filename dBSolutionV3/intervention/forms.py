ecaf06f8c46013116b953931582d2b74d43d290c
padeb185 <pierre-andre@hotmail.be>
2026-03-13 14:55:44 +0100

    Intervention carrosserie

--- dBSolutionV3/intervention/forms.py
+++ dBSolutionV3/intervention/forms.py
@@ -3,29 +3,68 @@ from django.forms import inlineformset_factory
 from .models import Intervention, MainOeuvre, Peinture, InterventionItem
 
 
+from django import forms
+from .models import Intervention, InterventionItem, MainOeuvre, Peinture, TypePieceCarrosserie
+
+
 class InterventionForm(forms.ModelForm):
     class Meta:
         model = Intervention
-        fields = ["societe", "voiture_exemplaire"]
+        fields = [
+            "societe",
+            "voiture_exemplaire",
+        ]
+        widgets = {
+            "societe": forms.Select(attrs={"class": "form-select"}),
+            "voiture_exemplaire": forms.Select(attrs={"class": "form-select"}),
+        }
 
 
 class InterventionItemForm(forms.ModelForm):
     class Meta:
         model = InterventionItem
-        fields = ["type", "reference", "quantite", "prix_unitaire", "taux_tva"]
+        fields = [
+            "type",
+            "reference",
+            "quantite",
+            "prix_unitaire",
+            "taux_tva",
+        ]
+        widgets = {
+            "type": forms.Select(choices=TypePieceCarrosserie.choices, attrs={"class": "form-select"}),
+            "reference": forms.TextInput(attrs={"class": "form-control"}),
+            "quantite": forms.NumberInput(attrs={"class": "form-control", "min": 1}),
+            "prix_unitaire": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+            "taux_tva": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+        }
 
 
 class MainOeuvreForm(forms.ModelForm):
     class Meta:
         model = MainOeuvre
-        fields = ["description", "heures", "taux_horaire"]
+        fields = [
+            "description",
+            "heures",
+            "taux_horaire",
+        ]
+        widgets = {
+            "description": forms.TextInput(attrs={"class": "form-control"}),
+            "heures": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+            "taux_horaire": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+        }
 
 
 class PeintureForm(forms.ModelForm):
     class Meta:
         model = Peinture
-        fields = ["zone", "prix"]
-
+        fields = [
+            "zone",
+            "prix",
+        ]
+        widgets = {
+            "zone": forms.TextInput(attrs={"class": "form-control"}),
+            "prix": forms.NumberInput(attrs={"class": "form-control", "step": "0.01"}),
+        }
 
 # Formsets
 InterventionItemFormSet = inlineformset_factory(
--- dBSolutionV3/intervention/models.py
+++ dBSolutionV3/intervention/models.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
