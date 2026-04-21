2dd673c029c533702accfae3d908a5fba3417694
padeb185 <pierre-andre@hotmail.be>
2026-04-21 14:53:58 +0200

    checkup track

--- dBSolutionV3/maintenance/checkup_track/views.py
+++ dBSolutionV3/maintenance/checkup_track/views.py
@@ -24,7 +24,7 @@ from .models import CheckupTrack
 class CheckupTrackListView(ListView):
     model = CheckupTrack
     template_name = "checkup_track/checkup_track_list.html"
-    context_object_name = "track_checks"
+    context_object_name = "checkup_tracks"
     paginate_by = 100
     ordering = ["-id"]
 
