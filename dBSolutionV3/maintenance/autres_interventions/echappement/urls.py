0dc60e2d00327ea3942b9d33a0578b925ade0cee
Pierre-André de Bournonville <pierre-andre@hotmail.be>
2026-07-13 10:10:56 +0200

    echappement

--- dBSolutionV3/maintenance/autres_interventions/echappement/urls.py
+++ dBSolutionV3/maintenance/autres_interventions/echappement/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import dashboard_echappement_view, EchappementListView
+from .views import dashboard_echappement_view, EchappementListView, echappement_check_view, modifier_echappement_view, \
+    echappement_detail_view, echappement_check_pdf_view
 
 app_name = "echappement"
 
@@ -13,13 +14,15 @@ urlpatterns = [
 
     path('echappement/<uuid:exemplaire_id>/liste/', EchappementListView.as_view(), name='echappement_list'),
 
+
     path('echappement/<uuid:exemplaire_id>/', echappement_check_view, name='echappement_check'),
 
 
-    path('boite/<int:boite_id>/modifier/', modifier_echappement_view, name='modifier_echappement'),
+    path('boite/<int:echappement_id>/modifier/', modifier_echappement_view, name='modifier_echappement'),
 
 
-    path('echappement/<int:boite_id>/detail/', echappement_detail_view, name='echappement_detail'),
+    path('echappement/<int:echappement_id>/detail/', echappement_detail_view, name='echappement_detail'),
+
 
     path("<int:pk>/", echappement_check_pdf_view, name="echappement_check_pdf"),
 
