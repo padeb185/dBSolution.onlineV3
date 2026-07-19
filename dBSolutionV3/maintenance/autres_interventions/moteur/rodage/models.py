62568e9123a4df335962ad5cc85df8474aded023
Pierre-André de Bournonville <pierre-andre@hotmail.be>
2026-07-19 10:04:03 +0200

    taux rodage

--- dBSolutionV3/maintenance/autres_interventions/moteur/rodage/models.py
+++ dBSolutionV3/maintenance/autres_interventions/moteur/rodage/models.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from maintenance.autres_interventions.moteur.admission.models import TAUX_HORAIRE_CHOICES
 from maintenance.check_up.models import PhareEtat
 from maintenance.choices import RouesSerrageEtat
 from utils.mixin import TechnicienMixin
@@ -306,6 +307,14 @@ class Rodage(TechnicienMixin, models.Model):
         blank=True,
         verbose_name=_("Société"),
         related_name="rodage_societe"
+    )
+
+    taux_horaire = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        choices=TAUX_HORAIRE_CHOICES,
+        default=Decimal("50.00"),
+        verbose_name=_("Taux horaire"),
     )
 
     # --- Date d'enregistrement ---
