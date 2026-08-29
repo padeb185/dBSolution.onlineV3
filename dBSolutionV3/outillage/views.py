b401c6f39f11e8cdc431f4f8697773429d12617c
Pierre-André de Bournonville <pierre-andre@hotmail.be>
2026-08-29 11:03:42 +0200

    corrections diverses

--- dBSolutionV3/outillage/views.py
+++ dBSolutionV3/outillage/views.py
@@ -15,8 +15,7 @@ class OutillageListView(ListView):
     model = Outillage
     template_name = "outillage/outillage_list.html"
     context_object_name = "outillages"
-    paginate_by = 20
-    ordering = ["nom"]
+    ordering = ["-id"]
 
     def get_queryset(self):
         societe = self.request.user.societe
